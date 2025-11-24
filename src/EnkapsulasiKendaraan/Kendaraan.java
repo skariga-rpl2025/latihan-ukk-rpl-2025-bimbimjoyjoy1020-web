8c03e335885cee763ccf69f41c450d823378936b
Bimbim Joevanka <bimbimjoyjoy1020@gmail.com>
2025-11-24 14:03:27 +0700

    EnkapsulasiKendaraan

---
+++ src/EnkapsulasiKendaraan/Kendaraan.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package EnkapsulasiKendaraan;
+
+/**
+ *
+ * @author jevann
+ */
+public class Kendaraan {
+    private String noPolisi;
+    private String merk;
+    private int tahun;
+
+    public String getNoPolisi() {
+        return noPolisi;
+    }
+
+    public void setNoPolisi(String noPolisi) {
+        this.noPolisi = noPolisi;
+    }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public int getTahun() {
+        return tahun;
+    }
+
+    public void setTahun(int tahun) {
+        this.tahun = tahun;
+    }
+}
